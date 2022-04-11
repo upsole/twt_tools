7b6d18ec12e3527f9d33c22cef5c212c9c66dcec
piotr <upsol@protonmail.com>
2022-04-11 19:24:27 +0200

    tests

--- tests/test_twt_tools.py
+++ tests/test_twt_tools.py
@@ -1,5 +1,16 @@
 from twt_tools import __version__
+from twt_tools.thread import Thread
+import os 
 
 
 def test_version():
     assert __version__ == '0.1.0'
+
+def test_thread_finishes():
+    name = '1513515770361397253'
+    thread = Thread(url=name, thread_name=name)
+    thread.build_markdown()
+    thread.build_pdf()
+    thread.cleanup()
+    os.remove(f'{name}.pdf')
+    assert True
